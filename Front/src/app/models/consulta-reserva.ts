624f062c033e0e84af35ea26eb5b787292998fea
daysigm9 <daysigm9@gmail.com>
2024-03-15 23:46:59 -0600

    cambios

--- Front/src/app/models/consulta-reserva.ts
+++ Front/src/app/models/consulta-reserva.ts
@@ -1,4 +1,12 @@
+
 export class ConsultaReserva {
-    origen: string = '';
-    destino: string ='';
-}
+    UsuarioId:number = 0;
+    SubEscalaOrigen:number = 0;
+    SubEscalaDestino:number=0; 
+    CantidadAsientos: number = 1;
+    ViajeId: number=0;
+    Precio: number=0;
+  }
+
+
+  
